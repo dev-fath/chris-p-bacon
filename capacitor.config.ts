bfd35c48bf2fde5ef017fc7b6d9ae8507628c481
DEVFATH <40345474+dev-fath@users.noreply.github.com>
2023-03-07 12:09:01 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.humaxmobility.evinfra',
+  appName: 'next-ev-infra-clone',
+  webDir: 'out',
+  bundledWebRuntime: false,
+  // server: {
+  //   url: 'http://localhost:3000'
+  // }
+};
+
+export default config;
